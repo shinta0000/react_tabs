d5cc39232ac7e520a493b65f50bc4827af71b403
ueno <uenoshinta0k@gmail.com>
2020-09-08 19:25:58 +0900

    Implement Header.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <div className="logo">
+        logo
+      </div>
+      <ul className="header-list">
+        <li>hoge</li>
+        <li>bar</li>
+        <li>foo</li>
+      </ul>
+    </header>
+  );
+}
+
+export { Header };
